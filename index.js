7ec8d4baeec2a086cd247d9f662e988369ff9b78
Bridget <bridget.njoki@student.moringaschool.com>
2024-07-25 22:52:51 +0300

    add

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
-
-    function clickAlert() {
-        alert('I was clicked!');
-    }
-    
-input.addingEventListener('click', clickAlert);
-}
-
+    const form = document.querySelector("form");
+    form.addEventListener("submit", submitForm);
+  }
+  
+  
+  function submitForm(event) {
+    event.preventDefault();  
+    console.log(document.querySelector("#create_item").value);  // Log the input value to the console
+  }
+  
+  
+addingEventListener();
+  
